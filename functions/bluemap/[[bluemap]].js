fd03c46bb113791a4a85e95a4c748982f2ba3110
Stefan Glorch <sglorch02@gmail.com>
2024-02-22 19:38:31 +0100

    Tried fixing proxy

--- functions/bluemap/[[bluemap]].js
+++ functions/bluemap/[[bluemap]].js
@@ -8,10 +8,11 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  const { url } = new URL(request.url);
+  const url  = new URL(request.url);
+  
   const newReq = new Request(request);
   newReq.headers.delete("Host");
   newReq.headers.append("Host", "minecraft.sglorch.de");
-  newReq.url = new URL("https://zora.sglorch.de/"+url.pathname)
+  newReq.url = new URL("https://zora.sglorch.de/" + url.pathname + url.search)
   return await fetch(newReq);
 }
